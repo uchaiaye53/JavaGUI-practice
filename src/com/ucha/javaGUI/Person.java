145423ed50b2070cf77c4f5f1deab935f2e10d1e
uchaiaye53 <uchhai53@gmail.com>
2022-01-13 18:14:15 +0600

    initial

---
+++ src/com/ucha/javaGUI/Person.java
@@ -0,0 +1,50 @@
+package com.ucha.javaGUI;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private  String email;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+
+
+    public Person(String name, String email, String phoneNumber, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
---
+++ src/com/ucha/javaGUI/ScreenOfContact.java
@@ -0,0 +1,18 @@
+package com.ucha.javaGUI;
+
+import javax.swing.*;
+
+public class ScreenOfContact {
+
+    private JPanel panelTop;
+    private JPanel panelLeft;
+    private JPanel panelRight;
+    private JList listOfPeople;
+    private JButton buttonNew;
+    private JButton buttonSave;
+    private JTextField textName;
+    private JTextField textMail;
+    private JTextField textPhn;
+    private JTextField textDOB;
+    private JLabel labelAge;
+}
